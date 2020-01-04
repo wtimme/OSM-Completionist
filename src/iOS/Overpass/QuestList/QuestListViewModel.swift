8908d6fcbfb41689c3c5ea21f8ceafd7a2658409
wtimme <wtimme@users.noreply.github.com>
2020-01-04 13:04:58 +0100

    #21 Add empty view model for quest list

---
+++ src/iOS/Overpass/QuestList/QuestListViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  QuestListViewModel.swift
+//  Go Map!!
+//
+//  Created by Wolfgang Timme on 1/4/20.
+//  Copyright © 2020 Bryce. All rights reserved.
+//
+
+class QuestListViewModel {
+
+}
