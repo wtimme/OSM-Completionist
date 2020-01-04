6593649c72e2a2360ce7c5e310ba077fd18f27a8
wtimme <wtimme@users.noreply.github.com>
2020-01-04 13:44:34 +0100

    #21 Add object for providing quests compiled into the app
    As per suggestion from @tordans, as an MVP, we will
    have static quests that we ship with the app.

---
+++ src/iOS/Overpass/QuestList/StaticQuestProvider.swift
@@ -0,0 +1,24 @@
+//
+//  StaticQuestProvider.swift
+//  Go Map!!
+//
+//  Created by Wolfgang Timme on 1/4/20.
+//  Copyright © 2020 Bryce. All rights reserved.
+//
+
+/// Provides static `Quest`s that are compiled into the app.
+class StaticQuestProvider {
+    private var accessibleToiletsQuest: Quest {
+        let question = "Are these toilets wheelchair accessible?"
+        let query = "(type:node or type:way) and amenity=toilets and access !~ \"private|customers\" and wheelchair!=*"
+        
+        return Quest(question: question,
+                     overpassWizardQuery: query)
+    }
+}
+
+extension StaticQuestProvider: QuestProviding {
+    var quests: [Quest] {
+        [accessibleToiletsQuest]
+    }
+}
