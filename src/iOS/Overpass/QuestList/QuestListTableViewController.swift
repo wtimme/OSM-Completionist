21ab1ae630ba1baf5aedd42d6d787ae5cd635cab
wtimme <wtimme@users.noreply.github.com>
2020-01-04 12:47:54 +0100

    #21 Add view controller for the list of quests

---
+++ src/iOS/Overpass/QuestList/QuestListTableViewController.swift
@@ -0,0 +1,29 @@
+//
+//  QuestListTableViewController.swift
+//  Go Map!!
+//
+//  Created by Wolfgang Timme on 1/4/20.
+//  Copyright © 2020 Bryce. All rights reserved.
+//
+
+import UIKit
+
+class QuestListTableViewController: UITableViewController {
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = NSLocalizedString("Quests",
+                                  comment: "Title of the quest list")
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+}
