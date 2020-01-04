3702ccb4bdb220e485acbd49ff9326e5ad59ff91
wtimme <wtimme@users.noreply.github.com>
2020-01-04 13:23:49 +0100

    #21 Add `Quest` data model

---
+++ src/iOS/Overpass/Quest.swift
@@ -0,0 +1,20 @@
+//
+//  Quest.swift
+//  Go Map!!
+//
+//  Created by Wolfgang Timme on 1/4/20.
+//  Copyright © 2020 Bryce. All rights reserved.
+//
+
+/// Represents a task/challenge that the user should fulfil.
+///
+/// The name stems from the Android app [StreetComplete](https://wiki.openstreetmap.org/wiki/StreetComplete/Quests).
+struct Quest {
+    /// The question that the user is asked when encountering a map item for this quest.
+    let question: String
+    
+    /// A query that is used to filter items that match this quest.
+    ///
+    /// Uses the [Overpass Turbo Wizard](https://wiki.openstreetmap.org/wiki/Overpass_turbo/Wizard) format.
+    let overpassWizardQuery: String
+}
