4c1de0b727bdbd42da766728582084896c05982c
wtimme <wtimme@users.noreply.github.com>
2020-01-04 13:46:19 +0100

    #21 Add mock implemention for `QuestProviding`

---
+++ src/iOS/GoMapTests/Mocks/QuestProviderMock.swift
@@ -0,0 +1,14 @@
+//
+//  QuestProviderMock.swift
+//  GoMapTests
+//
+//  Created by Wolfgang Timme on 1/4/20.
+//  Copyright © 2020 Bryce. All rights reserved.
+//
+
+import Foundation
+@testable import Go_Map__
+
+class QuestProviderMock: QuestProviding {
+    var quests = [Quest]()
+}
