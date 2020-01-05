5096cc04d4a06e3f5d0edbbb4a8dbb1af755d39a
wtimme <wtimme@users.noreply.github.com>
2020-01-05 14:03:01 +0100

    #21 Add extension on `Quest` for the static quests in the app
    This allows for the static quests that are compiled into
    the app to reside in one dedicated place, instead of having
    them pile up in `StaticQuestProvider`.

---
+++ src/iOS/Overpass/Quest+staticQuests.swift
@@ -0,0 +1,12 @@
+//
+//  Quest+staticQuests.swift
+//  Go Map!!
+//
+//  Created by Wolfgang Timme on 1/5/20.
+//  Copyright © 2020 Bryce. All rights reserved.
+//
+
+/// This extension contains `Quest`s that are compiled into the app.
+extension Quest {
+    
+}
