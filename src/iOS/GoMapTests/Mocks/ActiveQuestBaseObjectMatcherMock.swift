324cb1d083b7ac92c774ee0123994f279043808d
wtimme <wtimme@users.noreply.github.com>
2020-01-11 03:06:23 +0100

    #29 Add mock for `ActiveQuestBaseObjectMatching`

---
+++ src/iOS/GoMapTests/Mocks/ActiveQuestBaseObjectMatcherMock.swift
@@ -0,0 +1,20 @@
+//
+//  ActiveQuestBaseObjectMatcherMock.swift
+//  GoMapTests
+//
+//  Created by Wolfgang Timme on 1/11/20.
+//  Copyright © 2020 Bryce. All rights reserved.
+//
+
+import Foundation
+@testable import Go_Map__
+
+class ActiveQuestBaseObjectMatcherMock: ActiveQuestBaseObjectMatching {
+    private(set) var baseObject: OsmBaseObject?
+    var questsToReturn = [Quest]()
+    func quests(matching baseObject: OsmBaseObject) -> [Quest] {
+        self.baseObject = baseObject
+        
+        return questsToReturn
+    }
+}
