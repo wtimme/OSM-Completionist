c714d10416eb02755f1c761015650e5d1af269a7
wtimme <wtimme@users.noreply.github.com>
2020-01-11 01:23:18 +0100

    #29 Add integration test for the static quests
    This test makes sure that the static quests' queries were
    successfully parsed, so that they can be used by the user.

---
+++ src/iOS/GoMapTests/Overpass/Quest+staticQuestsTestCase.swift
@@ -0,0 +1,26 @@
+//
+//  Quest+staticQuestsTestCase.swift
+//  GoMapTests
+//
+//  Created by Wolfgang Timme on 1/11/20.
+//  Copyright © 2020 Bryce. All rights reserved.
+//
+
+import XCTest
+@testable import Go_Map__
+
+class Quest_staticQuestsTestCase: XCTestCase {
+
+    func testAllStaticQuests_shouldHaveBaseObjectMatcher() {
+        let allStaticQuests = [Quest.makeAccessibleToiletsQuest(),
+                               Quest.makeParkingFeeQuest(),
+                               Quest.makeBenchBackrestQuest(),
+                               Quest.makePlaygroundAccessQuest(),
+                               Quest.makeToiletQuest()]
+        
+        allStaticQuests.forEach { quest in
+            XCTAssertNotNil(quest.baseObjectMatcher)
+        }
+    }
+
+}
