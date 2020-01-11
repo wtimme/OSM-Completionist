9c1a0c0502dab8e96c54d2a6dedd5c645ed219b0
wtimme <wtimme@users.noreply.github.com>
2020-01-11 01:13:41 +0100

    #29 Add empty test case for `Quest`

---
+++ src/iOS/GoMapTests/Overpass/QuestTestCase.swift
@@ -0,0 +1,13 @@
+//
+//  QuestTestCase.swift
+//  GoMapTests
+//
+//  Created by Wolfgang Timme on 1/11/20.
+//  Copyright © 2020 Bryce. All rights reserved.
+//
+
+import XCTest
+@testable import Go_Map__
+
+class QuestTestCase: XCTestCase {
+}
