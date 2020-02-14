b16c4a91d37cf32a74dd8c60f681a05b40860aee
wtimme <wtimme@users.noreply.github.com>
2020-02-14 23:11:57 +0100

    #240 Add empty `MapRouletteLayerView`
    This new view will house the logic for displaying the
    MapRoulette annotations.

---
+++ src/iOS/Layers/MapRouletteLayerView.swift
@@ -0,0 +1,13 @@
+//
+//  MapRouletteLayerView.swift
+//  Go Map!!
+//
+//  Created by Wolfgang Timme on 2/14/20.
+//  Copyright © 2020 Bryce. All rights reserved.
+//
+
+import UIKit
+
+class MapRouletteLayerView: UIView {
+    
+}
