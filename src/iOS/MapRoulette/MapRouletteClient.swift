a770c5a6d2ae61becb8e7e6ba780ace0ac800569
wtimme <wtimme@users.noreply.github.com>
2020-03-06 18:09:30 +0100

    #240 Add empty `MapRouletteClient` class

---
+++ src/iOS/MapRoulette/MapRouletteClient.swift
@@ -0,0 +1,16 @@
+//
+//  MapRouletteClient.swift
+//  Go Map!!
+//
+//  Created by Wolfgang Timme on 3/6/20.
+//  Copyright © 2020 Bryce. All rights reserved.
+//
+
+import Foundation
+
+/// An object that communicates with the MapRoulette API.
+protocol MapRouletteClientProtocol {
+}
+
+final class MapRouletteClient: MapRouletteClientProtocol {
+}
